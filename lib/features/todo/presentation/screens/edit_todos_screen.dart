ed0196e0874f487169d77fd733336dec645bf0a6
nishanpradhan <nishanpradhan500@gmail.com>
2025-03-05 16:29:56 +0545

    Rename 'Add Task' to 'Edit Todos' and update task creation to support editing with improved time selection

--- lib/features/todo/presentation/screens/edit_todos_screen.dart
+++ lib/features/todo/presentation/screens/edit_todos_screen.dart
@@ -16,107 +16,94 @@ class EditTodosScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: CustomAppBar(
-        title: 'Add Task',
+        title: 'Edit Todos',
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            LabelWidget(
-              text: 'Title',
-            ),
-            CustomFormFieldWidget(
-              label: 'title',
-              controller: todosProvider.titleController,
-            ),
-            LabelWidget(
-              text: 'Description',
-            ),
-            CustomFormFieldWidget(
-              label: 'Descriptions',
-              vertical: 40,
-              controller: todosProvider.descriptionController,
-            ),
-            LabelWidget(
-              text: 'Time',
-            ),
-            TimePickerWidget(
-              onTimeSelected: (time) {
-                todosProvider.setTime(time); // Set the selected time
-              },
-            ),
-            CustomButton(
-              title: 'Create Task',
-              onPressed: () async {
-                // Create a TaskModel object with the selected time
-                final task = TodosModel(
-                  title: todosProvider.titleController.text,
-                  description: todosProvider.descriptionController.text,
-                  time: todosProvider.selectedTime, // Use the selected time
-                );
-                try {
-                  await todosProvider.createTask(task);
-                  todosProvider.titleController.clear();
-                  todosProvider.descriptionController.clear();
-                  todosProvider
-                      .resetTime(); // Reset the time after task creation
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Task created successfully!')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              LabelWidget(
+                text: 'Title',
+              ),
+              CustomFormFieldWidget(
+                label: 'title',
+                controller: todosProvider.titleController,
+              ),
+              LabelWidget(
+                text: 'Description',
+              ),
+              CustomFormFieldWidget(
+                label: 'Descriptions',
+                vertical: 40,
+                controller: todosProvider.descriptionController,
+              ),
+              LabelWidget(
+                text: 'Time',
+              ),
+              TimePickerWidget(),
+              SizedBox(
+                height: 10,
+              ),
+              CustomButton(
+                title: 'Update Todos',
+                onPressed: () async {
+                  // Create a TaskModel object with the selected time
+                  final task = TodosModel(
+                    title: todosProvider.titleController.text,
+                    description: todosProvider.descriptionController.text,
+                    time: todosProvider.selectedTime, // Use the selected time
                   );
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Failed to create task: $e')),
-                  );
-                }
-              },
-            ),
-          ],
+                  try {
+                    await todosProvider.createTask(task);
+                    todosProvider.titleController.clear();
+                    todosProvider.descriptionController.clear();
+                    todosProvider
+                        .resetTime(); // Reset the time after task creation
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Todos Updated successfully!')),
+                    );
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Failed to Update Todos: $e')),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-// Widget for selecting time
-class TimePickerWidget extends StatefulWidget {
-  final Function(String) onTimeSelected;
-
-  const TimePickerWidget({super.key, required this.onTimeSelected});
-
-  @override
-  _TimePickerWidgetState createState() => _TimePickerWidgetState();
-}
-
-class _TimePickerWidgetState extends State<TimePickerWidget> {
-  TimeOfDay _selectedTime = TimeOfDay.now();
-
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime,
-    );
-    if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
-        final formattedTime =
-            '${_selectedTime.hour}:${_selectedTime.minute.toString().padLeft(2, '0')} ${_selectedTime.period.name.toUpperCase()}';
-        widget.onTimeSelected(formattedTime); // Pass the formatted time
-      });
-    }
-  }
+class TimePickerWidget extends StatelessWidget {
+  const TimePickerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final todosProvider = Provider.of<TodosProvider>(context);
+
     return InkWell(
-      onTap: () => _selectTime(context),
+      onTap: () => todosProvider.timePicker(context),
       child: InputDecorator(
         decoration: InputDecoration(
-          labelText: 'Select Time',
-          border: OutlineInputBorder(),
+          hintText: 'Select Time',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Colors.black54),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Colors.black, width: 2),
+          ),
         ),
         child: Text(
-          '${_selectedTime.hour}:${_selectedTime.minute.toString().padLeft(2, '0')} ${_selectedTime.period.name.toUpperCase()}',
+          todosProvider.selectedTime.isEmpty
+              ? 'Select Time'
+              : todosProvider.selectedTime,
         ),
       ),
     );
