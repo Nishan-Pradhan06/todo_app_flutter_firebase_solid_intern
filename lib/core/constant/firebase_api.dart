6a4d0cb4f2881962eaac569f47d6ae0b7a4ae620
nishanpradhan <nishanpradhan500@gmail.com>
2025-03-06 16:12:44 +0545

    Integrate local notifications for Firebase Messaging and update AndroidManifest.xml

--- lib/core/constant/firebase_api.dart
+++ lib/core/constant/firebase_api.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:todo/main.dart';
 import '../../features/todo/presentation/screens/not_compelted_task_screen.dart';
 
@@ -12,12 +14,46 @@ Future<void> handleBackgroundMessage(RemoteMessage message) async {
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  //for to handle the notification when app is running state. bu adding the or using the flutter local notificaton package.
+
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for imporatant notification',
+    importance: Importance.defaultImportance,
+  );
+
+  //flutter local notification instance
+
+  final _localNotificatons = FlutterLocalNotificationsPlugin();
+
   void handleMessage(RemoteMessage? message) {
     if (message == null) return;
     navigatorKey.currentState?.pushNamed(
       InCompletedTodos.route,
       arguments: message,
     );
+  }
+
+Future initLocalNotifications() async {
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android);
+
+    await _localNotificatons.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+        // Handle notification tap
+        final payload = response.payload;
+        if (payload != null) {
+          final message = RemoteMessage.fromMap(jsonDecode(payload));
+          handleMessage(message);
+        }
+      },
+    );
+
+    final platform = _localNotificatons.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
   }
 
   Future initPushNotifications() async {
@@ -31,12 +67,32 @@ class FirebaseApi {
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+      _localNotificatons.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
   }
 
   Future<void> initNotifications() async {
     // Register the background message handler
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
     initPushNotifications();
+    initLocalNotifications();
 
     // Request permissions
     await _firebaseMessaging.requestPermission();
